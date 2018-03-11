0b776c3f5c2f6216a2a3f977db3052919e64508c
Michael Zoech <michi.zoech@gmail.com>
2018-03-10 22:52:40 -0800

    Use a FragmentStatePageAdapter

--- app/src/main/java/org/bitbrothers/shoppi/ui/activity/MainActivity.java
+++ app/src/main/java/org/bitbrothers/shoppi/ui/activity/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.StrictMode;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -45,7 +45,7 @@ public class MainActivity extends AppCompatActivity {
         tabLayout.setupWithViewPager(viewPager);
     }
 
-    private static class PagerAdapter extends FragmentPagerAdapter {
+    private static class PagerAdapter extends FragmentStatePagerAdapter {
 
         private final Context context;
 
