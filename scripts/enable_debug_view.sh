990f32f3493424667e426d1fa40a1eaff1334a6f
Michael Zoech <michi.zoech@gmail.com>
2018-03-16 14:20:42 -0700

    Use debug app in scripts

--- scripts/enable_debug_view.sh
+++ scripts/enable_debug_view.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-adb shell setprop debug.firebase.analytics.app org.bitbrothers.shoppi
+adb shell setprop debug.firebase.analytics.app org.bitbrothers.shoppi.debug
--- scripts/fill_test_data.sh
+++ scripts/fill_test_data.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-PACKAGE="org.bitbrothers.shoppi"
+PACKAGE="org.bitbrothers.shoppi.debug"
 adb $* shell run-as $PACKAGE sqlite3 /data/data/$PACKAGE/databases/main < $SCRIPT_DIR/testdata.sql
